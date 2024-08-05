2d402a7add9b630a3fa18eb0f59d24cf8458b2d3
esraaaatmeh <joudjarrah175@gmail.com>
2024-08-05 17:49:15 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(App());
+}
+class App extends StatelessWidget{
+  @override
+  Widget build (BuildContext context){
+    return MaterialApp(
+  debugShowCheckedModeBanner: false,
+      home: ChangeColor(),
+    );
+  }
+}
+
+class ChangeColor extends StatefulWidget{
+  @override
+  _ChangeColorState createState() => _ChangeColorState();
+}
+
+class _ChangeColorState extends State<ChangeColor>{
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+
+}
