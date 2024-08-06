3eec689f75ec0d5f548ec1233536e6d53f638474
esraaaatmeh <joudjarrah175@gmail.com>
2024-08-06 13:38:39 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,48 @@ class ChangeColor extends StatefulWidget{
 }
 
 class _ChangeColorState extends State<ChangeColor>{
+  Color _color = Colors.blueGrey;
+  Color _appColor = Colors.deepPurple;
   @override
   Widget build (BuildContext context){
     return Scaffold(
       appBar: AppBar(
         title: Text("Stateful Widget",
           style:TextStyle(color:Colors.white,fontWeight:FontWeight.bold),),
-        backgroundColor: Colors.deepPurple, ),
+        backgroundColor: _appColor, ),
+      body:
+      Container(margin:EdgeInsets.all(30) ,
+        child: Column(children:[
+          Row(children: [ElevatedButton(onPressed:()=>{setState(() {
+              _color = Colors.cyanAccent;
+              _appColor = Colors.black12;
+          })},
+          child: Text("Color 1",style:TextStyle(fontSize:20,fontWeight:FontWeight.bold,color:Colors.black87,)),
+          style:ElevatedButton.styleFrom(padding:EdgeInsets.all(20),backgroundColor:Colors.cyanAccent)),
+          SizedBox(width: 10,),
+            ElevatedButton(onPressed:()=>{
+              setState(() {
+                _color = Colors.white;
+                _appColor = Colors.black;
+
+              })
+            }, child: Text("Color 2",style:TextStyle(fontSize:20,fontWeight:FontWeight.bold,color:Colors.black87,)),
+              style: ElevatedButton.styleFrom(padding:EdgeInsets.all(20),backgroundColor:Colors.white,),),
+            SizedBox(width: 10,),
+            ElevatedButton(onPressed:()=>{
+              setState(() {
+                _color = Colors.white10;
+                _appColor = Colors.indigoAccent;
+
+              })
+            }, child: Text("Color 3",style:TextStyle(fontSize:20,fontWeight:FontWeight.bold,color:Colors.black87,)),
+              style:ElevatedButton.styleFrom(padding:EdgeInsets.all(20),backgroundColor:Colors.white10,),),
+          SizedBox(width: 10,)],
+          mainAxisAlignment:MainAxisAlignment.center,),
+        ],
+        ),
+      ),
+      backgroundColor: _color
     );
   }
 
